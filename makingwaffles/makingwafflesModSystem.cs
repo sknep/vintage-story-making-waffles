8f9a9b94268ea2a5bf7cd960401ca0921091d5a8
Sarah Rudder <sarah@sarahrudder.com>
2026-01-01 16:11:34 -0600

    init commit

---
+++ makingwaffles/makingwafflesModSystem.cs
@@ -0,0 +1,11 @@
+﻿using Vintagestory.API.Common;
+
+namespace MakingWaffles;
+
+public class MakingWafflesModSystem : ModSystem
+{
+    public override void Start(ICoreAPI api)
+    {
+        base.Start(api);
+    }
+}
